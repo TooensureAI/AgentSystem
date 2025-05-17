970f3d5778be61e5ee7dfcf88ef5178cd64b4e87
ShawnBellazan <ShawnBellazan@TOOENSURE>
2025-05-17 02:11:55 -0500

    Add project files.

---
+++ AgentSystem.Mobile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace AgentSystem.Mobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
